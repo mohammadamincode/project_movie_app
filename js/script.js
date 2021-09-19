4e70f5c31698ecfdffe00122a83344ea5bacc628
mohammadamincode <mohammadamincode@gmail.com>
2021-09-19 23:48:44 +0430

    Loading Animation

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ const next = document.getElementById("next");
 const up = document.getElementById("up");
 const header = document.getElementById("header");
 const pagination = document.getElementById("pagination");
+const loadingContainer = document.getElementById("loading-container");
 
 let currentPage = 1;
 let nextPage = 2;
@@ -43,6 +44,7 @@ const genres = [
 let selectedGenre = [];
 setGenre();
 function setGenre() {
+  loadingContainer.classList.remove("is-disabled");
   tagsEl.innerHTML = "";
   genres.forEach((genre) => {
     const t = document.createElement("div");
@@ -67,6 +69,7 @@ function setGenre() {
       highlightSelection();
     });
     tagsEl.append(t);
+    loadingContainer.classList.add("is-disabled");
   });
 }
 
@@ -90,10 +93,15 @@ function clearBtn() {
 
 getMovies(API_Url);
 function getMovies(url) {
+  header.scrollIntoView({
+    behavior: "smooth",
+  });
+  loadingContainer.classList.remove("is-disabled");
   lastUrl = url;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
+      loadingContainer.classList.add("is-disabled");
       console.log(data.results);
       if (data.results.length !== 0) {
         showMovies(data.results);
@@ -114,10 +122,6 @@ function getMovies(url) {
           prev.classList.remove("disabled");
           next.classList.remove("disabled");
         }
-
-        header.scrollIntoView({
-          behavior: "smooth",
-        });
       } else {
         main.innerHTML = `
         <h1 class="no-result">No Results Found</h1>
@@ -135,6 +139,7 @@ function getMovies(url) {
 }
 
 function showMovies(data) {
+  loadingContainer.classList.remove("is-disabled");
   main.innerHTML = "";
 
   data.forEach((movie) => {
@@ -173,6 +178,7 @@ function showMovies(data) {
           </div>
       `;
     main.appendChild(movieEl);
+    loadingContainer.classList.add("is-disabled");
     pagination.classList.remove("is-disabled");
   });
 }
@@ -239,6 +245,9 @@ prev.addEventListener("click", () => {
   }
 });
 function pageCall(page) {
+  header.scrollIntoView({
+    behavior: "smooth",
+  });
   let urlSplit = lastUrl.split("?");
   let queryParams = urlSplit[1].split("&");
   let key = queryParams[queryParams.length - 1].split("=");
