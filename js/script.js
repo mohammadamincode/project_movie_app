cccc9b63d745c449bf210623fa8f34b71712a99a
mohammadamincode <mohammadamincode@gmail.com>
2021-09-25 22:54:16 +0330

    Update v2.0

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ const up = document.getElementById("up");
 const header = document.getElementById("header");
 const pagination = document.getElementById("pagination");
 const loadingContainer = document.getElementById("loading-container");
+const home = document.getElementById("home");
 
 let currentPage = 1;
 let nextPage = 2;
@@ -199,6 +200,7 @@ form.addEventListener("submit", (e) => {
   setGenre();
   if (searchTerm) {
     getMovies(searchURL + "&query=" + searchTerm);
+    home.classList.remove("is-disabled");
   } else {
     getMovies(API_Url);
   }
@@ -265,4 +267,11 @@ up.addEventListener("click", () => {
   header.scrollIntoView({
     behavior: "smooth",
   });
+});
+home.addEventListener("click", () => {
+  search.value = "";
+  selectedGenre = [];
+  setGenre();
+  getMovies(API_Url);
+  home.classList.add("is-disabled");
 });
